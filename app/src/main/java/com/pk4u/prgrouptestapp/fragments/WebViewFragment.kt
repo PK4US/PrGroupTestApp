93440cd48fdd08d876011f318e996462793ef72a
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-17 00:51:19 +0300

    save page until close

--- app/src/main/java/com/pk4u/prgrouptestapp/fragments/WebViewFragment.kt
+++ app/src/main/java/com/pk4u/prgrouptestapp/fragments/WebViewFragment.kt
@@ -1,6 +1,8 @@
 package com.pk4u.prgrouptestapp.fragments
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.LayoutInflater
@@ -15,10 +17,13 @@ import com.pk4u.prgrouptestapp.databinding.FragmentWebViewBinding
 
 open class WebViewFragment : Fragment() {
     private lateinit var binding: FragmentWebViewBinding
+    private lateinit var prefs: SharedPreferences
+    private val APP_PREFERENCES_WEB_URL = "app_pref_web_url"
+    private var webUrl = "https://megasport.ua/"
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentWebViewBinding.inflate(inflater,container,false)
+        prefs = requireContext().getSharedPreferences("settings", Context.MODE_PRIVATE)
         opeWebView()
         internet()
         return binding.root
@@ -27,7 +32,7 @@ open class WebViewFragment : Fragment() {
     @SuppressLint("SetJavaScriptEnabled")
     private fun opeWebView() = with(binding){
         webView.webViewClient = WebViewClient()
-        webView.loadUrl("https://megasport.ua/")
+        webView.loadUrl(webUrl)
         webView.settings.javaScriptEnabled = true
         webView.settings.setSupportZoom(true)
 
@@ -46,6 +51,28 @@ open class WebViewFragment : Fragment() {
             }
         })
     }
+
+    override fun onPause() {
+        super.onPause()
+        webUrl = binding.webView.url.toString()
+        // Запоминаем данные
+        val editor = prefs.edit()
+        editor.putString(APP_PREFERENCES_WEB_URL, webUrl).apply()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if(prefs.contains(APP_PREFERENCES_WEB_URL)){
+            // Получаем число из настроек
+            webUrl = prefs.getString(APP_PREFERENCES_WEB_URL, "g").toString()
+
+
+            binding.webView.webViewClient = WebViewClient()
+            binding.webView.loadUrl(webUrl)
+        }
+    }
+
 
     private fun internet(){
         val networkConnection = context?.let { NetworkConnection(it) }
